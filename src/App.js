93fac6a1f9495c5dec3e471fa4eb26de07a33f74
nimitagarwal26 <agarwalnimit88@gmail.com>
2023-10-08 00:25:14 +0530

    added pages and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from "./Components/Header";
+import Homepage from './Pages/Homepage';
+import CoinPage from './Pages/CoinPage';
+import { makeStyles } from "@material-ui/core";
 
 function App() {
+
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: "#14161a",
+    }
+  }));
+  const classes = useStyles()
   return (
-    <div className="App">
-      hello world
-    </div>
+    <Router>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path='/'Component={Homepage} exact />
+          <Route path='/coins/:id'Component={CoinPage} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Pages/CoinPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CoinPage = () => {
+  return (
+    <div>Coin Page</div>
+  )
+}
+
+export default CoinPage

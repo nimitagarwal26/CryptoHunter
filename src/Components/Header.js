b06739a9c7a230f4c8b5bad958bda65355c367a7
Vinayak <vinshr3@gmail.com>
2023-10-12 23:24:40 +0530

    created currency conversion list and give some styling in header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,29 @@
-import { AppBar, Container, Toolbar, Typography } from '@mui/material'
+import { AppBar, Container,Select, MenuItem, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import { styled } from "@mui/material";
+
+
+const AppRoot = styled('div')(({ theme }) => ({
+  flex:1,
+  color:"white",
+  fontFamily: 'Montserrat, sans-serif',
+  fontSize:'22px',
+  fontWeight:'800',
+  cursor:'pointer',
+}));
 
 const Header = () => {
   return (
    <AppBar color='primary' position='static'>
       <Container>
         <Toolbar>
-          <Typography>Crypto Hunter</Typography>
+
+          <AppRoot><Typography><AppRoot>Crypto Hunter</AppRoot></Typography></AppRoot>
+          <Select variant='outlined'  style={{width:100,height:40,marginLeft:15 }}>
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"INR"}>INR</MenuItem>
+           </Select>
+           
         </Toolbar>
       </Container>
      

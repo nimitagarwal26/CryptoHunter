a3cfbde9d8ba458e6614a6c4b1e57c221541e8f0
nimitagarwal26 <agarwalnimit88@gmail.com>
2023-11-11 00:50:40 +0530

    updated carousel

--- src/Components/Banner/Carousel.js
+++ src/Components/Banner/Carousel.js
@@ -1,81 +1,3 @@
-// // 
-
-
-// import { styled } from '@mui/material/styles';
-// import axios from 'axios';
-// import React, { useEffect, useState } from 'react';
-// import { TrendingCoins } from '../../config/api';
-// import { CryptoState } from '../../CryptoContext';
-// import AliceCarousel from 'react-alice-carousel';
-// import { Link } from 'react-router-dom';
-
-// const AppRoot = styled('div')(({ theme }) => ({
-//   height: '50%',
-//   display: 'flex',
-//   alignItems: 'center',
-// }));
-
-// const Carousel = () => {
-//   const [trending, setTrending] = useState([]);
-//   const { currency } = CryptoState();
-//   const fetchTrendingCoins = async () => {
-//     const { data } = await axios.get(TrendingCoins(currency));
-//     setTrending(data);
-//   };
-//   console.log(trending);
-//   useEffect(() => {
-//     fetchTrendingCoins();
-//   }, [currency]);
-
-//   const items = trending.map((coin) => (
-//     let profit = coin?.price_change_percentage_24h>=0,
-//     <Link key={coin.id} to={`/coins/${coin.id}`} style={{ textDecoration: 'none', marginRight: 10, color: 'inherit' }}>
-//       <img
-//         src={coin?.image}
-//         alt={coin.name}
-//         height="90"
-//         style={{ marginBottom: 10 }}
-//       />
-//       <span>
-//         {coin?.symbol}
-//         &nbsp;
-//         <span>
-//           {profit && "+"}
-//           {coin?.price_change_percentage_24h?.toFixed(2)}%
-//         </span>
-//       </span>
-//     </Link>
-//   ));
-
-//   const responsive = {
-//     0: {
-//       items: 2,
-//     },
-//     512: {
-//       items: 4,
-//     },
-//   };
-
-//   return (
-//     <AppRoot>
-//       <AliceCarousel
-//         mouseTracking
-//         infinite
-//         autoPlayInterval={1000}
-//         animationDuration={1500}
-//         disableDotsControls
-//         disableButtonsControls
-//         responsive={responsive}
-//         autoPlay
-//         items={items}
-//       />
-//     </AppRoot>
-//   );
-// };
-
-// export default Carousel;
-
-
 import { styled } from '@mui/material/styles';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
@@ -90,8 +12,6 @@ const AppRoot = styled('div')(({ theme }) => ({
   alignItems: 'center',
 }));
 
-// Import makeStyles from Material-UI
-//import { makeStyles } from '@mui/styles';
 
 const Carousel = () => {
   const [trending, setTrending] = useState([]);
